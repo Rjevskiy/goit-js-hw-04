6c706933ea55cf8b860c93361bce907142da2884
Alexsander Rjevskiy <porezkakir2@gmail.com>
2024-09-15 14:25:11 +0300

    3

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,34 @@
-// changeUsername(newName)
+
+const profile = {
+  username: 'Jacob',
+  playTime: 300,
+    
+    changeUsername(newName) {
+    this.username = newName;
+  },
+
+    updatePlayTime(active) {
+    this.playTime += active;
+  },
+
+    getInfo() {
+    return `${this.username} has ${this.playTime} active hours!`;
+  },
+};
+
+//let profile1 = Object.values(profile)
+
+//console.log (`${profile1.slice(0,1)} has ${profile1.slice(1,2)} active hours!`)
+
+console.log(`${profile.username} has ${profile.playTime} active hours!`);  
 
 
 
-// // console.log(profile.getInfo()); // "Jacob has 300 active hours!"
 
-// // profile.changeUsername("Marco");
-// // console.log(profile.getInfo()); // "Marco has 300 active hours!"
+console.log(profile); // "Jacob has 300 active hours!"
 
-// // profile.updatePlayTime(20);
-// // console.log(profile.getInfo()); // "Marco has 320 active hours!"
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
